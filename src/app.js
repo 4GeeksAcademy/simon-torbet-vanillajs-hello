56055b4eba764dcdf5e8ce59488b8796e0b51bb6
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2023-09-06 22:51:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".Card2").classList.add(GenerateRandomCards());
+  document.querySelector(".Card-number").innerHTML = GenerateRandomNumber();
 };
+function GenerateRandomNumber() {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let Numero = Math.floor(Math.random() * numeros.length);
+  return numeros[Numero];
+}
+function GenerateRandomCards() {
+  let cards = ["diam", "spade", "heart", "club"];
+  let card = Math.floor(Math.random() * cards.length);
+  return cards[card];
+}
